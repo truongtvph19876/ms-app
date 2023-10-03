15cb6bd0ffad02af66118ce9b008ea57b9aa4d31
truongtvph19876 <truongtv67891@gmail.com>
2023-10-03 09:54:43 +0700

    first

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import msData from './data/data.json' assert { type: "json" };
-
+import { msIcon } from './include.js';
 // get dom element
 const rootApp = document.querySelector('#root');
 const msContainer = rootApp.querySelector('#ms-container')
 
 const { list } = msData;
 
-const msContainerData = list.map((item)=> {
-     return `<li data-ms-id=${item.id}>${item.msName}</li>`;
+// ms state
+const msState = false;
+
+const msContainerData = list.map((item, index)=> {
+     return `<li data-ms-id=${item.id}># ${index + 1} ${item.msName}</li>`;
 }).join('')
 
 msContainer.innerHTML = msContainerData;
@@ -23,10 +26,13 @@ listMs.forEach((ms)=> {
           const msId = item.target.dataset.msId;
           const [audioProperty] = list.filter((item => item.id == msId));
           
-          const msSrc = `${msRootPath}${audioProperty.msName}.${audioProperty.msExt}`;
+          const msSrc = `${msRootPath}${audioProperty.src}.${audioProperty.msExt}`;
           currentAudio.src = msSrc;
           currentAudio.play();
      })
 })
 
+
+// const stateElement = document.querySelector('#state');
+// stateElement.innerHTML=msIcon.play;
 
